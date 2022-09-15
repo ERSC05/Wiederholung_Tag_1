bec1bcfd5ff8217a7093156eb38b54450d31fc8e
Schaeberle, Erwin <erwin.schaeberle@tecalliance.net>
2022-09-15 12:55:05 +0200

    Aufgabe 3 #3

--- Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
+++ Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
@@ -10,12 +10,22 @@ namespace Wiederholung_tag_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Sage mir eine Zahl ");
-            int Zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sag mir einen Text. U und G wird vertauscht. ");
+            string Text = Console.ReadLine();
+            string TextAnders = Text.Replace('U', 'G').Replace('u', 'g');
+            
+             
+
+            Console.WriteLine(TextAnders);
+            
+            
+            
+ /*           int Zahl1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Sage mir eine weitere Zahl ");
             int Zahl2 = Convert.ToInt32(Console.ReadLine());
             int Ergebnis = Zahl1 + Zahl2;
             Console.WriteLine(Zahl1+" + "+Zahl2+" = "+Ergebnis);
+ */
 
             Console.ReadLine();
 
