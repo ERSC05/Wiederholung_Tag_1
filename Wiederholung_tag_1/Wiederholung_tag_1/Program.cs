358fd83b0f3034f05676eac77fa9d228f7a65d0a
Schaeberle, Erwin <erwin.schaeberle@tecalliance.net>
2022-09-16 10:13:52 +0200

    Tachenrechner #21

--- Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
+++ Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
@@ -11,36 +11,42 @@ namespace Wiederholung_tag_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Nenne die Größe vom int-Array");
-            int Groese = Convert.ToInt32(Console.ReadLine());
-            int[] zahlen = new int[Groese];
-
-
-            for (int i= 0; i < zahlen.Length; i++)
-            {
-                Console.WriteLine("gebe mir eine Zahl für Array!");
-                zahlen[i] = Convert.ToInt32(Console.ReadLine());
-            }
-            Console.WriteLine("Deine Zahlen sind");
-            Console.Write("[");
-
-            for (int i = 0; i < zahlen.Length; i++)
-            {
-                Console.Write($"{zahlen[i]} | ");
-            }
-            Console.Write("]");
-            Console.ReadLine();
-
+            Console.WriteLine("Nenne mir eine Zahl");
+            double zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nenne mir eine weitere Zahl");
+            double zahl2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nenne mir einen Opperrator");
+            string operrator = Console.ReadLine();
+            double sum = zahl1 + zahl2;
+            double sup = zahl1 - zahl2;
+            double pro = zahl1 * zahl2;
+            double div = zahl1 / zahl2;
+            double mod = zahl1 % zahl2;
 
             
 
-            Console.Write("Minimum und maximum sind hier:");
-            Console.Write(" "+zahlen.Min() + " , ");
-            Console.WriteLine(zahlen.Max());
+            switch (operrator)
+            {
+                case "+":
+                    Console.WriteLine(zahl1+"+"+zahl2+"="+sum);
+                    break;
+                case "-":
+                    Console.WriteLine(zahl1 + "-" + zahl2 + "=" +sup);
+                    break;
+                case "*":
+                    Console.WriteLine(zahl1 + "*" + zahl2 + "=" + pro);
+                    break;
+                case "/":
+                    Console.WriteLine(zahl1 + "/" + zahl2 + "=" + div);
+                    break;
+                case "%":
+                    Console.WriteLine(zahl1 + "%" + zahl2 + "=" + mod);
+                    break;
 
-            Console.WriteLine("Der durschnitt beträgt: "+zahlen.Average());
-
-
+                default:
+                    Console.WriteLine("das war kein opperrater >:(");
+                        break;
+            }
 
 
             Console.ReadLine();
