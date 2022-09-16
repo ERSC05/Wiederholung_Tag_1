96ba1128b5d284b0cc612bb0e9b454c6142e6f6d
Schaeberle, Erwin <erwin.schaeberle@tecalliance.net>
2022-09-16 11:00:09 +0200

    Listen Aufgabe 1 #23

--- Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
+++ Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
@@ -12,7 +12,22 @@ namespace Wiederholung_tag_1
         static void Main(string[] args)
         {
             List<string> namen = new List<string>();
+            Console.WriteLine("Wie viele namen willst du aufzählen?");
+            int anzahl = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < anzahl; i++)
+            {
+                Console.WriteLine("Nenne einen Namen");
+                namen.Add(Console.ReadLine());
+                Console.WriteLine(namen[i]);
+            }
+            Console.WriteLine("Okay das sind also deinen Namne:");
 
+            for (int i = 0; i < namen.Count; i++)
+            {
+                Console.Write(namen[i]+" | ");
+
+            }
             Console.ReadLine();
 
 
