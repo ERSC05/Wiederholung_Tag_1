f0cf3d172f7ff87cef59c001207df3f45d209b87
Schaeberle, Erwin <erwin.schaeberle@tecalliance.net>
2022-09-16 14:13:15 +0200

    Start 2.Woche Nr 2 und 3 und 4 #25

--- Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
+++ Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
@@ -11,58 +11,31 @@ namespace Wiederholung_tag_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("gebe ein zahl ein");
-            int zahl1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Gebe eine weitere Zahl ein");
-            int zahl2 = Convert.ToInt32(Console.ReadLine());
+            List<int> list1 = new List<int>() {5, 3, 7};
+            string space = Console.ReadLine();
+            string n = Console.ReadLine();
+            
+            do
+            {
+                Console.WriteLine("füge zahlen hinzu ");
+                list1.Add(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("willst du noch eine andere Zahl hinzufügen? ");
+                if (n == "n")
+                {
+                    Console.WriteLine("Füge weitere Zahlen hinzu");
+            
+                
+                }
+            } while (Console.ReadLine() != "n");
 
-            int ergebnis = Add(zahl1, zahl2);
-            Console.WriteLine(ergebnis);
+            foreach (int i in list1)
+            {
+                Console.WriteLine(i);
+            }
 
-            ergebnis = divide(zahl1, zahl2);
-            Console.WriteLine(ergebnis);
-
-            ergebnis = subtra(zahl1, zahl2);
-            Console.WriteLine(ergebnis);
-
-            ergebnis = multi(zahl1, zahl2);
-            Console.WriteLine(ergebnis);
 
             Console.ReadLine();
-        }
-
-        static int Add(int z1, int z2)
-        {
-            int summe = z1 + z2;
-            return summe;
-        }
-        static int divide(int z1, int z2)
-        {
-            int div = z1 / z2;
-            return div;
-        }
-
-        static int subtra(int z1, int z2)
-        {
-            int sub = z1 - z2;
-            return sub;
-        }
-        static int multi(int z1, int z2)
-        {
-            int mult = z1 * z2;
-            return mult;
-        }
+        }        
         
-
-
-        //static Rückgabetyp Methodenname (Parameter)
-        static List<string> Number()
-        {
-            List<string> name = new List<string>();
-            name.Add("ABD");
-
-            //Rückgabevariable
-            return name;
-        }
     }
 }
