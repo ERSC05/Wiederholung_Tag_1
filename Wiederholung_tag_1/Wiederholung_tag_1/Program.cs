8873c775fb2f387a754c1f63482bc61b600c8105
Schaeberle, Erwin <erwin.schaeberle@tecalliance.net>
2022-09-16 10:46:48 +0200

    Liste csharp #22

--- Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
+++ Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
@@ -11,46 +11,9 @@ namespace Wiederholung_tag_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Nenne mir eine Zahl");
-            double zahl1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Nenne mir eine weitere Zahl");
-            double zahl2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Nenne mir einen Opperrator");
-            string operrator = Console.ReadLine();
-            double sum = zahl1 + zahl2;
-            double sup = zahl1 - zahl2;
-            double pro = zahl1 * zahl2;
-            double div = zahl1 / zahl2;
-            double mod = zahl1 % zahl2;
-
-            
-
-            switch (operrator)
-            {
-                case "+":
-                    Console.WriteLine(zahl1+"+"+zahl2+"="+sum);
-                    break;
-                case "-":
-                    Console.WriteLine(zahl1 + "-" + zahl2 + "=" +sup);
-                    break;
-                case "*":
-                    Console.WriteLine(zahl1 + "*" + zahl2 + "=" + pro);
-                    break;
-                case "/":
-                    Console.WriteLine(zahl1 + "/" + zahl2 + "=" + div);
-                    break;
-                case "%":
-                    Console.WriteLine(zahl1 + "%" + zahl2 + "=" + mod);
-                    break;
-
-                default:
-                    Console.WriteLine("das war kein opperrater >:(");
-                        break;
-            }
-
+            List<string> namen = new List<string>();
 
             Console.ReadLine();
-
 
 
         }
