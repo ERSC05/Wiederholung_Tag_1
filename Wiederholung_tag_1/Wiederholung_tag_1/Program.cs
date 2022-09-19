db958d2b1c5adf84f9c23328c1424ac6877b997f
Schaeberle, Erwin <erwin.schaeberle@tecalliance.net>
2022-09-19 08:47:52 +0200

    #26

--- Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
+++ Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -12,30 +13,51 @@ namespace Wiederholung_tag_1
     {
         static void Main(string[] args)
         {
-            int Pair = 0;
+            int Pair = 1;
             int Ja = 0;
 
             Console.WriteLine("Drücke 1 um das spiel zu starten");
             Ja = Convert.ToInt32(Console.ReadLine());
-            Random r = new Random(36);
-            int gerade = r % 2;
+            Random r = new Random();
+            int gerade = 2;
+            int ungerade = 1;
+            int t = 2;
+            int k = r.Next(0, 37);
+            int guthaben = 10000;
+            
 
-            while (Ja == 1)
+            //            int mod = 2;
+
+            if (Ja == 1)
             {
-                if (Pair == 1)
+                Pair = k;
+
+                switch (Pair)
                 {
-                    if (r == gerade)
-                    {
-                        Console.WriteLine("DU gewinnst $20 bei deinem nächsten match")
-                    }
-                    else if (r == 0)
-                    {
-                        Console.WriteLine("Dein einsatz bleibt gleich")
-                    }
+                    case 1:
+                        t = gerade;
+                        Console.WriteLine("dein Gewinn erhöt sich");
+                        break;
+                    case 2:
+                        t = ungerade;
+                        Console.WriteLine("Dein einsatz bleibt bestehen");
+                        break;
+                    case 3:
+                        t = 0;
+                        Console.WriteLine("Du hast dein Einsatzt verloren");
+                        break;
+                    default:
+                        Console.WriteLine(r);
+                        break;
+
+
                 }
-
+                
             }
-
+            else
+            {
+                Console.WriteLine("dann gibt es kein spiel");
+            }
             Console.ReadLine();
         }        
         
