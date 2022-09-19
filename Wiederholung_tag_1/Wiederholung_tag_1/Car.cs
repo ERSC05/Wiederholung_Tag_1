afb555f84abd0bc807d6a6b5fa3f7c78df1e83c2
Schaeberle, Erwin <erwin.schaeberle@tecalliance.net>
2022-09-19 10:48:47 +0200

    Car classen #27

--- Wiederholung_tag_1/Wiederholung_tag_1/Car.cs
+++ Wiederholung_tag_1/Wiederholung_tag_1/Car.cs
@@ -9,9 +9,28 @@ namespace Wiederholung_tag_1
     public class Car
     {
         public int TypeId { get; set; }
-        public string make { get; set; }
+        public string Make { get; set; }
         public string Model { get; set; }
-        public int TypeName { get; set; }  
+        public string TypeName { get; set; }  
         public string Color { get; set; }
+        public int CCM { get; set; }
+        public int Reifen { get; set; }
+
+
+        public Car(int typeId, string make, string model, string typeName, string color, int ccm, int reifen)
+        {
+            TypeId = typeId;
+            Make = make;
+            Model = model;
+            TypeName = typeName;
+            Color = color;
+            CCM = ccm;
+            Reifen = reifen;
+        }
+
+        public void PrintCarInformation()
+        {
+            Console.WriteLine("mein Fahrzeug ist "  + " " + Model + " " + TypeName + "mit einem Hubraum von" + TypeId + "in der Farbe" + Color + "mit den reifen");
+        }
     }
 }
--- Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
+++ Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Management.Instrumentation;
+using System.Net.Mime;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -14,14 +15,64 @@ namespace Wiederholung_tag_1
     {
         static void Main(string[] args)
         {
-            Car c1 = new Car();
-            c1.TypeId = 13494;
-            c1.make = "VW";
-            c1.Model = "vw gti";
-            c1.TypeName = 5;
-            c1.Color = "red";
+            Console.WriteLine("Wie viele Autos willst du in die Liste anlegen?");
 
-            Console.ReadLine();
+            
+            int AutoZahl = Convert.ToInt32(Console.ReadLine());
+            int TypeId = 0;
+            string Make = "0";
+            string Model = "0";
+            string Color = "0";
+            string typeName = "0";
+            int CCM = 0;
+            int Reifen = 0;
+            int f = 0;
+
+
+
+            List<Car> cars = new List<Car>() {};
+            while (AutoZahl != f)
+            {
+                Console.WriteLine("gib eine TypeId an");
+                TypeId = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Make eingeben");
+                Make = Console.ReadLine();
+                Console.WriteLine("Model eingeben");
+                Model = Console.ReadLine();
+                Console.WriteLine("typeName eingeben");
+                typeName = Console.ReadLine();
+                Console.WriteLine("Color eingeben");
+                Color = Console.ReadLine();
+                Console.WriteLine("ccm eingeben");
+                CCM = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Reifen anzahl");
+                Reifen = Convert.ToInt16(Console.ReadLine());
+
+
+                f = f + 1;
+                Car c = new Car(TypeId, Make, Model, typeName, Color, CCM, Reifen);
+                cars.Add(c);
+
+
+            }
+            foreach(Car car in cars)
+            {
+                car.PrintCarInformation();
+            }
+            
+
+            
+
+/*            List<string> Tags = new List<string>(4);
+
+            List<Car> cars = new List<Car>();
+
+            foreach(Car car in cars)
+            {
+                
+                car.PrintCarInformation();
+            }  
+*/            Console.ReadLine();
         }        
         
     }
