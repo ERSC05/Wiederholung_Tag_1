5bd9c36bbeb1567531291807c2e54c59d55c1b3a
Schaeberle, Erwin <erwin.schaeberle@tecalliance.net>
2022-09-19 09:16:43 +0200

    Klassen #27

---
+++ Wiederholung_tag_1/Wiederholung_tag_1/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiederholung_tag_1
+{
+    public class Car
+    {
+        public int TypeId { get; set; }
+        public string make { get; set; }
+        public string Model { get; set; }
+        public int TypeName { get; set; }  
+        public string Color { get; set; }
+    }
+}
--- Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
+++ Wiederholung_tag_1/Wiederholung_tag_1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Management.Instrumentation;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -13,53 +14,17 @@ namespace Wiederholung_tag_1
     {
         static void Main(string[] args)
         {
-            int Pair = 1;
-            int Ja = 0;
+            Car c1 = new Car();
+            c1.TypeId = 13494;
+            c1.make = "VW";
+            c1.Model = "vw gti";
+            c1.TypeName = 5;
+            c1.Color = "red";
 
-            Console.WriteLine("Drücke 1 um das spiel zu starten");
-            Ja = Convert.ToInt32(Console.ReadLine());
-            Random r = new Random();
-            int gerade = 2;
-            int ungerade = 1;
-            int t = 2;
-            int k = r.Next(0, 37);
-            int guthaben = 10000;
-            
-
-            //            int mod = 2;
-
-            if (Ja == 1)
-            {
-                Pair = k;
-
-                switch (Pair)
-                {
-                    case 1:
-                        t = gerade;
-                        Console.WriteLine("dein Gewinn erhöt sich");
-                        break;
-                    case 2:
-                        t = ungerade;
-                        Console.WriteLine("Dein einsatz bleibt bestehen");
-                        break;
-                    case 3:
-                        t = 0;
-                        Console.WriteLine("Du hast dein Einsatzt verloren");
-                        break;
-                    default:
-                        Console.WriteLine(r);
-                        break;
-
-
-                }
-                
-            }
-            else
-            {
-                Console.WriteLine("dann gibt es kein spiel");
-            }
             Console.ReadLine();
         }        
         
     }
+
+    
 }
